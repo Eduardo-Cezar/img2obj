ec8dc52f6ca3ec9a95d5e36d75a297df8a30cbf9
Eduardo-Cezar <edcezar442@gmail.com>
2025-01-30 14:40:13 -0300

    :books: Atualizando dependencias

--- dependencias.sh
+++ dependencias.sh
@@ -7,3 +7,5 @@ git clone --depth 1 https://github.com/axinc-ai/ailia-models
 pip3 install -r ailia-models/neural_rendering/tripo_sr/requirements.txt
 
 pip3 install onnxruntime
+
+pip install flask flask-sqlalchemy psycopg2-binary werkzeug
